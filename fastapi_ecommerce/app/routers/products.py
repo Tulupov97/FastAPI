038fc04f511b32ed7902e07c2e7637a54e7a20a2
Tulupov Kirill <kirill.tulupov@mail.ru>
2026-01-06 03:23:43 +0300

    pet

--- fastapi_ecommerce/app/routers/products.py
+++ fastapi_ecommerce/app/routers/products.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, status, Depends, HTTPException
+from app.schemas import Product as ProductSchema, ProductCreate
+from app.db_depends import get_db
+from sqlalchemy.orm import Session
+from app.models.categories import Category as CategoryModel
+from app.models.products import Product as ProductModel
+from sqlalchemy import select, update
 
 # Создаём маршрутизатор для товаров
 router = APIRouter(
@@ -8,28 +13,42 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def get_all_products():
+@router.get("/", response_model=list[ProductSchema])
+async def get_all_products(dp: Session = Depends(get_db)):
     """
     Возвращает список всех товаров.
     """
-    return {"message": "Список всех товаров (заглушка)"}
+    products  = select(ProductModel).where(ProductModel.is_active == True)
+    return dp.scalars(products).all()
 
 
-@router.post("/")
-async def create_product():
+@router.post("/", response_model=ProductSchema, status_code=status.HTTP_201_CREATED)
+async def create_product(product: ProductCreate, db: Session = Depends(get_db)):
     """
     Создаёт новый товар.
     """
-    return {"message": "Товар создан (заглушка)"}
+    # Проверяем, существует ли активная категория
+    category = db.scalars(
+        select(CategoryModel).where(CategoryModel.id == product.category_id,
+                                    CategoryModel.is_active == True)
+    ).first()
+    if not category:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Category not found or inactive")
+        
+    db_product = ProductModel(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+    
 
-
-@router.get("/category/{category_id}")
-async def get_products_by_category(category_id: int):
+@router.get("/category/{category_id}", response_model=ProductSchema, status_code=status.HTTP_200_OK)
+async def get_products_by_category(res,category_id: int):
     """
     Возвращает список товаров в указанной категории по её ID.
     """
-    return {"message": f"Товары в категории {category_id} (заглушка)"}
+    return {"message": f"Детали товара {category_id} (заглушка)"}
 
 
 @router.get("/{product_id}")
