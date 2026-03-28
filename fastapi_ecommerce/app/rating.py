c117dd4d163591bed44c380c48d3d5ab50cb1ddd
Kirill <kirill.tulupov@mail.ru>
2026-03-29 01:15:54 +0300

    модуль высчитывания рейтинга

---
+++ fastapi_ecommerce/app/rating.py
@@ -0,0 +1,19 @@
+from sqlalchemy.sql import func
+from sqlalchemy import select
+from app.models.reviews import Review as ReviewModel
+from app.models.products import Product as ProductModel
+from app.db_depends import get_async_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
+
+async def update_product_rating(product_id: int, db: AsyncSession):
+    result = await db.execute(
+        select(func.avg(ReviewModel.grade)).where(
+            ReviewModel.product_id == product_id,
+            ReviewModel.is_active == True
+        )
+    )
+    avg_rating = result.scalar() or 0.0
+    product = await db.get(ProductModel, product_id)
+    product.rating = avg_rating
+    await db.commit()
