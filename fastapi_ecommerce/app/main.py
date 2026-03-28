a2aabd60f2cfe865b8a81eab4d24a8db650db7dd
Kirill <kirill.tulupov@mail.ru>
2026-03-29 01:14:38 +0300

    роутер на отзывы

--- fastapi_ecommerce/app/main.py
+++ fastapi_ecommerce/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.routers import categories, products, users
+from app.routers import categories, products, users, reviews
 
 app = FastAPI(title='FastAPI e-shop', version='0.1.0')
 
 app.include_router(categories.router)
 app.include_router(products.router)
 app.include_router(users.router)
+app.include_router(reviews.router)
 
 @app.get("/")
 async def root():
