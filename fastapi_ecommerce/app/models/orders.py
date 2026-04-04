dcdbaa86621d345207b048dbde068b9ade097472
kirill_tulupov <kirill.tulupov@mail.ru>
2026-04-04 22:18:55 +0300

    .

--- fastapi_ecommerce/app/models/orders.py
+++ fastapi_ecommerce/app/models/orders.py
@@ -1,11 +1,12 @@
 from app.database import Base
 from sqlalchemy.orm import mapped_column, Mapped
-
+from sqlalchemy import Integer, Numeric, String, Boolean
+from decimal import Decimal
 
 class Order(Base):
     __tablename__ = "orders"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    user_id: Mapped[int] = mapped_column(index=True)
-    total_price: Mapped[int] = mapped_column()
-    status: Mapped[str] = mapped_column()
-    is_active: Mapped[bool] = mapped_column(default=True, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(Integer, index=True)
+    total_price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    status: Mapped[str] = mapped_column(String(20))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, index=True)
