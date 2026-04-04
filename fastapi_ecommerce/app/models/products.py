1203afeb34ea34f602cea3bc7bb2e6b0603c838c
Tulupov Kirill <kirill.tulupov@mail.ru>
2026-04-05 02:36:15 +0300

    добавлен tsv поля

--- fastapi_ecommerce/app/models/products.py
+++ fastapi_ecommerce/app/models/products.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
-from sqlalchemy import String, Boolean, Integer, Numeric, Float, text
+from sqlalchemy import String, Boolean, Integer, Numeric, Float, text, Computed, Index
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
@@ -21,3 +22,17 @@ class Product(Base):
 
     category: Mapped["Category"] = relationship("Category", back_populates="products")
     seller: Mapped["User"] = relationship("User", back_populates="products")
+
+    tsv: Mapped[TSVECTOR] = mapped_column(
+        TSVECTOR,
+        Computed(
+            """
+            setweight(to_tsvector('english', coalesce(name, '')), 'A')
+            || 
+            setweight(to_tsvector('english', coalesce(description, '')), 'B')
+            """,
+            persisted=True,
+        ),
+        nullable=False,
+    )
+    __table_args__ = (Index("ix_products_tsv", tsv, postgresql_using="gin"),)
