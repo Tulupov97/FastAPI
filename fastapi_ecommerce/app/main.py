6b60a1749be749028c43f2420c78839489ab35ec
kirill_tulupov <kirill.tulupov@mail.ru>
2026-04-13 03:53:11 +0300

    добавлен мидлвар на логер

--- fastapi_ecommerce/app/main.py
+++ fastapi_ecommerce/app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import categories, products, users, reviews, orders, cart
 from fastapi.staticfiles import StaticFiles
+from app.logger.logger import log_middleware
 
 
 app = FastAPI(title='FastAPI e-shop', version='0.1.0')
@@ -11,6 +12,9 @@ app.include_router(users.router)
 app.include_router(reviews.router)
 app.include_router(orders.router)
 app.include_router(cart.router)
+app.middleware("http")(log_middleware)
+
+
 
 @app.get("/")
 async def root():
